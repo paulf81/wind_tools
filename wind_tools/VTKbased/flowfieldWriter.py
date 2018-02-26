b605e1f804399d878d794ef404085b59f17f6157
Roald Storm <roaldstorm@gmail.com>
2018-02-26 16:11:22 +0100

    Added the option to read .vtk files that use polydata or unstructured grid data

--- wind_tools/VTKbased/flowfieldWriter.py
+++ wind_tools/VTKbased/flowfieldWriter.py
@@ -99,3 +99,123 @@ def create_df_from_vti(fileName):
     df['z'] = pts[:, 2]
 
     return df, spacing, dimensions, origin
+
+
+def create_df_from_vtk_poly(fileName):
+    """Read flow array output from SOWFA
+
+
+    input:
+        filename: name of .vtk file to open with a 2D flowfield slice specified
+        as PolyData
+
+    output:
+        df: a pandas table with the columns, x,y,z,u,v,w of all relevant flow info
+        spacing: The spacing in the x, y and z direction between points
+        dimensions: a tuple or list of integers with the number of points in xyz
+        origin: the origin of the flow field, for reconstructing turbine coords
+
+    Roald Storm, 2018 """
+
+    reader = vtk.vtkGenericDataObjectReader()
+    reader.SetFileName(fileName)
+    reader.Update()
+    readerOutput = reader.GetOutput()
+
+    if not reader.IsFilePolyData():
+        raise ValueError('This .vtk file does not have PolyData')
+
+    if readerOutput.GetCell(0).GetPoints().GetData().GetNumberOfTuples() != 4:
+        raise ValueError('This .vtk file is not a 2d flowfield slice')
+
+    ncells = readerOutput.GetNumberOfCells()
+    cellData = readerOutput.GetCellData()
+
+    pts = np.zeros([ncells, 3])
+    vals = np.zeros([ncells, 3])
+    data = cellData.GetArray(0)
+
+    for i in range(data.GetNumberOfTuples()):
+        vals[i] = data.GetTuple(i)
+        pts[i] = [min(i) for i in zip(*[readerOutput.GetCell(i).GetPoints().GetData().GetTuple(x) for x in range(4)])]
+
+    df = DataFrame(columns=('x', 'y', 'z', 'u', 'v', 'w'))
+
+    df['u'] = vals[:, 0]
+    df['v'] = vals[:, 1]
+    df['w'] = vals[:, 2]
+    df['x'] = pts[:, 0]
+    df['y'] = pts[:, 1]
+    df['z'] = pts[:, 2]
+
+    df = df.sort_values(by=['z', 'y', 'x']).reset_index(drop=True)
+
+    dimensions = (len(df[(df['y'] == 0)].index),
+                  len(df[(df['x'] == 0)].index),
+                  1)
+    spacing = (df[df['x'] != 0].iloc[0].loc['x'],
+               df[df['y'] != 0].iloc[0].loc['y'],
+               0)
+    origin = (0.0, 0.0, 0.0)
+
+    return df, spacing, dimensions, origin
+
+
+def create_df_from_vtk_unstructuredgrid(fileName):
+    """Read flow array output from SOWFA
+
+
+    input:
+        filename: name of .vtk file to open with 3D flowfield specified as an
+        unstructured grid
+
+    output:
+        df: a pandas table with the columns, x,y,z,u,v,w of all relevant flow info
+        spacing: The spacing in the x, y and z direction between points
+        dimensions: a tuple or list of integers with the number of points in xyz
+        origin: the origin of the flow field, for reconstructing turbine coords
+
+    Roald Storm, 2018 """
+
+    reader = vtk.vtkGenericDataObjectReader()
+    reader.SetFileName(fileName)
+    reader.Update()
+    readerOutput = reader.GetOutput()
+
+    if not reader.IsFileUnstructuredGrid():
+        raise ValueError('This .vtk file does not have Unstructured Grid Data')
+
+    if readerOutput.GetCell(0).GetPoints().GetData().GetNumberOfTuples() != 8:
+        raise ValueError('This .vtk file is not a 3d flowfield slice')
+
+    ncells = readerOutput.GetNumberOfCells()
+    cellData = readerOutput.GetCellData()
+
+    pts = np.zeros([ncells, 3])
+    vals = np.zeros([ncells, 3])
+    data = cellData.GetArray(1)
+
+    for i in range(data.GetNumberOfTuples()):
+        vals[i] = data.GetTuple(i)
+        pts[i] = [min(i) for i in zip(*[readerOutput.GetCell(i).GetPoints().GetData().GetTuple(x) for x in range(8)])]
+
+    df = DataFrame(columns=('x', 'y', 'z', 'u', 'v', 'w'))
+
+    df['u'] = vals[:, 0]
+    df['v'] = vals[:, 1]
+    df['w'] = vals[:, 2]
+    df['x'] = pts[:, 0]
+    df['y'] = pts[:, 1]
+    df['z'] = pts[:, 2]
+
+    df = df.sort_values(by=['z', 'y', 'x']).reset_index(drop=True)
+
+    dimensions = (len(df[(df['y'] == 0) & (df['z'] == 0)].index),
+                  len(df[(df['x'] == 0) & (df['z'] == 0)].index),
+                  len(df[(df['x'] == 0) & (df['y'] == 0)].index))
+    spacing = (df[df['x'] != 0].iloc[0].loc['x'],
+               df[df['y'] != 0].iloc[0].loc['y'],
+               df[df['z'] != 0].iloc[0].loc['z'])
+    origin = (0.0, 0.0, 0.0)
+
+    return df, spacing, dimensions, origin
